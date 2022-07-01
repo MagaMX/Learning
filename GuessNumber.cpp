2b7fe22002c5cfee47b0953ab4414b76cc84be3b
MagaMX <mr.filippov2001@gmail.com>
2022-07-01 22:44:45 +0300

    Learning

---
+++ GuessNumber.cpp
@@ -0,0 +1,110 @@
+﻿#include <iostream>
+#include <ctime>
+#include <vector>
+
+double getRandom()
+{
+    int max = 999;
+    int min = 100;
+    static const double fraction = 1.0 / (static_cast<double>(RAND_MAX) + 1.0);
+    return static_cast<int>(rand() * fraction * (max - min + 1) + min);
+}
+
+std::vector<int> numToVector(int num)
+{
+    std::vector<int> vec;
+    vec.push_back(num / 100);
+    vec.push_back((num % 100) / 10);
+    vec.push_back(num % 10);
+    return vec;
+}
+
+int input()
+{
+    int x;
+    do
+    {
+        std::cin >> x;
+        if (std::cin.fail())
+        {
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            std::cout << "Неверный ввод! Повторите попытку.\nВаш вариант: ";
+        }
+    } while (std::cin.fail() || x < 100 || x > 999);
+    return x;
+}
+
+bool again()
+{
+    char answer;
+    do
+    {
+        std::cout << "Сыграть еще раз (1 - да/ 2 - нет)? ";
+        std::cin >> answer;
+        if (answer == '1')
+            return true;
+        if (answer == '2')
+            return false;
+    } while (answer != '1' && answer != '2');
+}
+
+bool game(std::vector<int> numComputer)
+{
+    std::vector<int> numPlayer;
+    int guesses = 0;
+    for (int k = 1; k <= 10; k++)
+    {
+        int rightnum = 0;
+        int rightplace = 0;
+        std::cout << "Ваш вариант: ";
+        int guess = input();
+        numPlayer = numToVector(guess);
+        guesses++;
+       
+        if (numComputer == numPlayer)
+        {
+            std::cout << "Вы угадали!\nКоличество попыток: " << guesses << std::endl;
+            return true;
+        }
+
+        for (int i = 0; i < numComputer.size(); ++i)
+        {
+            for (int j = 0; j < numPlayer.size(); ++j)
+            {
+                if (numComputer[i] == numPlayer[j])
+                {
+                    ++rightnum;
+                    break;
+                }   
+            }
+            if (numComputer[i] == numPlayer[i])
+            {
+                ++rightplace;
+            }
+        }
+        std::cout <<"Угадано: " << rightnum <<". На своих местах: " << rightplace<<".\n";
+    }
+    return false;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    srand(static_cast<unsigned int>(time(0)));
+    rand();
+    do
+    {
+        std::cout << "Компьютер загадал случайное 3-х значное число. Вы должны его отгадать. "
+            "После очередного числа вам будет сообщено, сколько цифр угадано"
+            " и сколько из них находится на своих местах. У вас 10 попыток.\n";
+        int number = getRandom();
+        std::vector <int> numComputer = numToVector(number);
+        std::cout << number;
+        if (!game(numComputer))
+            std::cout << "Извините вы не угадали!\nЗагаданное число: " << number << ".\n";
+    } while (again());
+ 
+    return 0;
+};
---
+++ MorzeToRus.cpp
@@ -0,0 +1,101 @@
+﻿
+#include <iostream>
+#include <vector>
+#include <string>
+#include <Windows.h>
+#include <cctype>
+#include <algorithm>
+using namespace std;
+
+struct Morze
+{
+    char letter;
+    string code;
+};
+
+void letterToMorze(const char letter,vector<Morze> alphabet)
+{
+    for (const auto element : alphabet)
+    {
+        if (element.letter == letter)
+        {
+            cout << element.code;
+        }
+    }
+}
+
+void textToLetter(string &text, vector<Morze> alphabet)
+{
+    for (const auto& element : text)
+    {
+        if (element == ' ') 
+            cout << "   ";
+        else
+            letterToMorze(element, alphabet);
+    }
+}
+
+bool again()
+{
+    cout << "\nХотите перевести еще одно сообщение(да/нет): ";
+    char answer;
+    cin >> answer;
+    if (answer == 'да')
+        return true;
+    else
+        return false;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "ru-RU");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    string text;
+    cout << "Введите сообщение: ";
+    getline(cin, text);
+    transform(text.begin(), text.end(), text.begin(), toupper);
+
+    vector<Morze>alphabet {
+        {'А', "._ "},
+        {'Б', "_... "},
+        {'В', ".__ "},
+        {'Г', "__. "},
+        {'Д', "_.. "},
+        {'Е', ". "},
+        {'Ж', "...- "},
+        {'З', "--.. "},
+        {'И', ".. "},
+        {'Й', ".___ "},
+        {'К', "_._ "},
+        {'Л', "._.. "},
+        {'М', "__ "},
+        {'Н', "_. "},
+        {'О', "___ "},
+        {'П', ".__. "},
+        {'Р', "._. "},
+        {'С', "... "},
+        {'Т', "_ "},
+        {'У', ".._ "},
+        {'Ф', ".._. "},
+        {'Х', ".... "},
+        {'Ц', "._._ "},
+        {'Ч', "___. "},
+        {'Ш', "____ "},
+        {'Щ', "__._ "},
+        {'Ъ', ".__._. "},
+        {'Ы', "_.__ "},
+        {'Ь', "_.._ "},
+        {'Э', ".._.. "},
+        {'Ю', "..__ "},
+        {'Я', "._._ "},
+    };
+    
+    do
+    {
+        textToLetter(text, alphabet);
+    } while (again());
+    
+    return 0;
+}
